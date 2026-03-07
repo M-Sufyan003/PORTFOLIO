10caa4b337547668c792d7eb4f0cdc493c73ca66
M-Sufyan003 <muhammadg115511@gmail.com>
2026-03-07 21:25:46 +0500

    Relative Path Issues for Projects Thumbnails on Deployment is Resolved

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -4,7 +4,7 @@ const projectsData = [
     title: "Weather App",
     description:
       "A sleek and responsive weather app built using ReactJS that displays real-time weather data based on user location or city input. It provides temperature, conditions, and icons with a clean UI.",
-    image: "public/projectIMGs/Weather_App.png",
+    image: "/projectIMGs/Weather_App.png",
     imgAlt: "Weather App",
     link: "https://weather-app-by-sufyan.netlify.app/",
   },
@@ -13,7 +13,7 @@ const projectsData = [
     title: "Calculator App",
     description:
       "Designed and developed a fully functional, interactive calculator using JavaScript, HTML5, and CSS3.",
-    image: "public/projectIMGs/Calculator_App.png",
+    image: "/projectIMGs/Calculator_App.png",
     imgAlt: "Calculator App",
     link: "https://calculator-by-sufyan.netlify.app/",
   },
@@ -22,7 +22,7 @@ const projectsData = [
     title: "Note It Down",
     description:
       "A clean and functional To-Do List app built with HTML, CSS, and JavaScript. Allows users to add, delete, and mark tasks as completed with real-time updates.",
-    image: "public/projectIMGs/To_Do_App.png",
+    image: "/projectIMGs/To_Do_App.png",
     imgAlt: "Note It Down App",
     link: "https://note-it-down-project.netlify.app/",
   },
@@ -31,7 +31,7 @@ const projectsData = [
     title: "Rock Paper Scissors Game",
     description:
       "A simple and interactive Rock Paper Scissors game built using HTML, CSS, and JavaScript. Features real-time score tracking, user vs computer gameplay, and smooth animations.",
-    image: "public/projectIMGs/Rock_Paper_Sciessor_App.png",
+    image: "/projectIMGs/Rock_Paper_Sciessor_App.png",
     imgAlt: "Rock Paper Scissors",
     link: "https://rockpapersciessor.netlify.app/",
   },
@@ -40,7 +40,7 @@ const projectsData = [
     title: "Expense Tracker Web App",
     description:
       "Responsive Website built using ReactJS for the purpose of managing day to day Budgets and monitor Cash Flow.",
-    image: "public/projectIMGs/ExpenseTracker_App.png",
+    image: "/projectIMGs/ExpenseTracker_App.png",
     imgAlt: "Expense Tracker",
     link: "https://expensetracker-by-sufyan.netlify.app/",
   },
@@ -49,7 +49,7 @@ const projectsData = [
     title: "Domain Generator",
     description:
       "Domain Generator is a website built using ReactJS aims for the purpose of suggesting custom Domains to user based on their input.",
-    image: "public/projectIMGs/DomainGenerator_App.png",
+    image: "/projectIMGs/DomainGenerator_App.png",
     imgAlt: "Domain Generator",
     link: "https://domaingenerator-by-sufyan.netlify.app/",
   },
@@ -58,7 +58,7 @@ const projectsData = [
     title: "QR Code Generator",
     description:
       "QR Code Generator is a Web App crafted with ReactJS for the purpose of generating QR Codes from any text as desired by Client.",
-    image: "public/projectIMGs/QRCodeGenerator.png",
+    image: "/projectIMGs/QRCodeGenerator.png",
     imgAlt: "QR Code Generator",
     link: "https://qrcodegenerator-by-sufyan.netlify.app/",
   },
@@ -67,7 +67,7 @@ const projectsData = [
     title: "React Forms",
     description:
       "This Project is developed in ReactJs to showcase my abilities of handling forms in ReactJS.",
-    image: "public/projectIMGs/React_Forms_App.png",
+    image: "/projectIMGs/React_Forms_App.png",
     imgAlt: "React Forms",
     link: "https://reactforms-by-sufyan.netlify.app/",
   },
@@ -76,7 +76,7 @@ const projectsData = [
     title: "Wheels On Demand",
     description:
       "Wheels on Demand is a full-stack web-based Vehicle rental system designed to simplify vehicle booking and management for both customers and administrators. Developed using ReactJS (frontend), Laravel(backend), and MySQL (database), the system ensures seamless API-driven integration and a secure,scalable architecture.",
-    image: "public/projectIMGs/WheelsOnDemand.png",
+    image: "/projectIMGs/WheelsOnDemand.png",
     imgAlt: "Wheels On Demand",
     link: "",
   },
